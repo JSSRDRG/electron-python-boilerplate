a6b1dade662f43fc2709f13a73710e2a226eda2d
Jesse Rodrigo <39565615+JSSRDRG@users.noreply.github.com>
2020-05-05 22:47:01 +0200

    Also print to console

--- src/photon.py
+++ src/photon.py
@@ -1,3 +1,12 @@
 import module.py.ipc as ipc
 import module.py.document as document
 import module.py.console as console
+
+import sys
+
+def print (text:any = '') :
+    '''
+         Print to shell
+    '''
+    sys.stdout.write(text)
+    console.log(str(text))
