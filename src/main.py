e76ee28e019289ef29ba786a13466b0532a051db
Jesse Rodrigo <39565615+JSSRDRG@users.noreply.github.com>
2020-05-05 19:28:38 +0200

    IPC test

--- src/main.py
+++ src/main.py
@@ -1,3 +1,12 @@
 from photon import console
+import time
 
-console.log('Hello World!')
+x = 0
+
+while True:
+    console.log(str(x))
+    console.warn(str(x))
+    console.error(str(x))
+    x += 1
+    time.sleep(1)
+
