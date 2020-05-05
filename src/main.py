3f611d38dc3a4833fb761b35a6a4fc5f8b0e5788
Jesse Rodrigo <39565615+JSSRDRG@users.noreply.github.com>
2020-05-05 22:47:37 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 from photon import console
+from photon import document
 import time
+
+import photon
+print = photon.print
 
 x = 0
 
@@ -7,6 +11,8 @@ while True:
     console.log(str(x))
     console.warn(str(x))
     console.error(str(x))
+    print('Hello World!')
+    console.log(str(document.anchors))
     x += 1
     time.sleep(1)
 
