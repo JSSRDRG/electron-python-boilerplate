848d83d76ef3cfe94ad00244088a9572a63ff224
Jesse Rodrigo <39565615+JSSRDRG@users.noreply.github.com>
2020-05-05 19:29:07 +0200

    Linted

--- src/module/py/console.py
+++ src/module/py/console.py
@@ -4,25 +4,25 @@
 
 import module.py.ipc as ipc
 
-def error ( text: str = "") :
+def error ( text:str = "") :
     '''
         Outputs an error message.
     '''
     ipc.send('{"f":"err","args":"' + text + '"}')
 
-def info ( text: str = "") :
+def info ( text:str = "") :
     '''
         Informative logging of information.
     '''
     ipc.send('{"f":"info","args":"' + text + '"}')
 
-def log ( text: str = "") :
+def log ( text:str = "") :
     '''
         For general output of logging information.
     '''
     ipc.send('{"f":"log","args":"' + text + '"}')
 
-def warn ( text: str = "") :
+def warn ( text:str = "") :
     '''
         Outputs a warning message.
     '''
