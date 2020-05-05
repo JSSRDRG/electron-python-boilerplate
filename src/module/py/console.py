2b441343644a7aa0195e75680292a4c6bcf4a718
Jesse Rodrigo <39565615+JSSRDRG@users.noreply.github.com>
2020-05-06 00:12:19 +0200

    IPC packet format update

--- src/module/py/console.py
+++ src/module/py/console.py
@@ -8,25 +8,25 @@ def error ( text:str = "") :
     '''
         Outputs an error message.
     '''
-    ipc.send('{"f":"err","args":"' + text + '"}')
+    ipc.send('{"obj":"console","func":"error","args":"' + text + '"}')
 
 def info ( text:str = "") :
     '''
         Informative logging of information.
     '''
-    ipc.send('{"f":"info","args":"' + text + '"}')
+    ipc.send('{"obj":"console","func":"info","args":"' + text + '"}')
 
 def log ( text:str = "") :
     '''
         For general output of logging information.
     '''
-    ipc.send('{"f":"log","args":"' + text + '"}')
+    ipc.send('{"obj":"console","func":"log","args":"' + text + '"}')
 
 def warn ( text:str = "") :
     '''
         Outputs a warning message.
     '''
-    ipc.send('{"f":"warn","args":"' + text + '"}')
+    ipc.send('{"obj":"console","func":"warn","args":"' + text + '"}')
 
 # TODO: Add all methods from the Javascript console object 
 # https://developer.mozilla.org/en-US/docs/Web/API/Console
